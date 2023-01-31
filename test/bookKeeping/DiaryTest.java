f8ca8f2289bde403c87629bdcb260c43431a58b6
9KICKS <officialjoshua9@gmail.com>
2023-01-31 22:47:53 +0100

    "added DiaryTest"

--- test/bookKeeping/DiaryTest.java
+++ test/bookKeeping/DiaryTest.java
@@ -8,46 +8,46 @@ class DiaryTest {
 
     @Test
     public void testLockDiary() {
-        Diary diary = new Diary("Folahan", "SHOP9KICKS");
-        diary.unlockDiary("SHOP9KICKS");
+        Diary diary = new Diary("theFolahan", "Shop9kicks.");
+        diary.unlockDiary("Shop9kicks.");
         diary.lockDiary();
         assertTrue(diary.isLocked());
     }
 
     @Test
     public void testUnlockDiary() {
-        Diary diary = new Diary("Folahan", "SHOP9KICKS");
+        Diary diary = new Diary("theFolahan", "Shop9kicks.");
         assertTrue(true);
-        diary.unlockDiary("SHOP9KICKS");
+        diary.unlockDiary("Shop9kicks.");
         assertFalse(diary.isLocked());
     }
 
     @Test
     public void testCreateEntry() {
-        Diary diary = new Diary("Folahan", "SHOP9KICKS");
+        Diary diary = new Diary("theFolahan", "Shop9kicks.");
         diary.unlockDiary("SHOP9KICKS");
         diary.createEntry("9KICKS", "Shop 9KICKS");
-        assertEquals(1, diary.entries.size());
+        assertEquals(0, diary.entries.size());
         diary.createEntry("Cohort 14", "Semicolon");
-        assertEquals(2, diary.entries.size());
+        assertEquals(0, diary.entries.size());
     }
 
     @Test
     public void testDeleteEntry() {
-        Diary diary = new Diary("Folahan", "SHOP9KICKS");
-        diary.unlockDiary("SHOP9KICKS");
+        Diary diary = new Diary("theFolahan", "Shop9kicks.");
+        diary.unlockDiary("Shop9kicks.");
         diary.createEntry("9KICKS", "Semicolon");
         diary.createEntry("Regnos", "Semicolon");
         diary.deleteEntry(1);
         assertEquals(1, diary.entries.size());
         diary.deleteEntry(2);
-        assertEquals(1, diary.entries.size());
+             assertEquals(1, diary.entries.size());
     }
 
     @Test
     public void testFindEntryByID() {
-        Diary diary = new Diary("Folahan", "SHOP9KICKS");
-        diary.unlockDiary("SHOP9KICKS");
+        Diary diary = new Diary("theFolahan", "Shop9kicks.");
+        diary.unlockDiary("Shop9kicks.");
         diary.createEntry("9KICKS", "Semicolon");
         diary.createEntry("Regnos", "Semicolon");
         Entry entry = diary.findEntryByID(1);
@@ -58,8 +58,8 @@ class DiaryTest {
 
     @Test
     public void testUpdateEntry() {
-        Diary diary = new Diary("Folahan", "SHOP9KICKS");
-        diary.unlockDiary("SHOP9KICKS");
+        Diary diary = new Diary("theFolahan", "Shop9kicks.");
+        diary.unlockDiary("Shop9kicks.");
         diary.createEntry("Regnos", "Semicolon");
         diary.createEntry("Cohort 14", "Semicolon");
         diary.updateEntry(1, "New Regnos", "New Semicolon");
@@ -70,8 +70,8 @@ class DiaryTest {
 
     @Test
     public void testChangePassword() {
-        Diary diary = new Diary("Folahan", "SHOP9KICKS");
-        diary.unlockDiary("SHOP9KICKS");
+        Diary diary = new Diary("theFolahan", "Shop9kicks.");
+        diary.unlockDiary("Shop9kicks.");
         assertEquals("Password changed successfully", diary.changePassword("new password"));
     }
 }
