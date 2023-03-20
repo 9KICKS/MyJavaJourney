0c21b411dfb1fc1cec3cbf6380b93ee2ecd42bbb
9KICKS <officialjoshua9@gmail.com>
2023-03-20 09:52:36 +0100

    "added chapter 14"

---
+++ src/chapterFourteen/StringExampleFive.java
@@ -0,0 +1,11 @@
+package chapterFourteen;
+
+public class StringExampleFive {
+    public static void main(String[] args) {
+        String name = "Folahan";
+        String tempValue = name;
+        name += "Joshua";
+        System.out.println(name);
+        System.out.println(tempValue);
+    }
+}
