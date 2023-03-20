cfae90a1cacc54744e08e444ee2bb838ce24bd8b
9KICKS <officialjoshua9@gmail.com>
2023-03-20 12:12:06 +0100

    "added chapter 14"

---
+++ src/chapterFourteen/StringExampleEight_RegularExpressions.java
@@ -0,0 +1,17 @@
+package chapterFourteen;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringExampleEight_RegularExpressions {
+    public static void main(String[] args) {
+        String userInput = "email@gmail.com";
+        System.out.println(userInput.matches("[a-z]+@[a-z]+\\.[a-z]{2,}"));
+
+        System.out.println(Pattern.matches("[a-z]+@[a-z]+\\.[a-z]{2,}", userInput));
+
+        Pattern pattern = Pattern.compile("[a-z]+@[a-z]+\\.[a-z]{2,}");
+        Matcher matcher = pattern.matcher(userInput);
+        System.out.println(matcher.matches());
+    }
+}
