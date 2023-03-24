736adb284b985da88f60b70a57d979b2cd09488c
9KICKS <officialjoshua9@gmail.com>
2023-03-24 18:40:19 +0100

    "updated chapter fifteen"

---
+++ src/chapterFifteen/serialize/Dog.java
@@ -0,0 +1,38 @@
+package chapterFifteen.serialize;
+
+import java.io.Serializable;
+
+public class Dog implements Serializable {
+    private static final long serialVersionUID =- 7316804528876700573L;
+    private String name;
+    private int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
