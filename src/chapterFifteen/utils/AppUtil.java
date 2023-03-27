7bbf7eef1a5cae46f230445b1cc4bcc44400ea3a
9KICKS <officialjoshua9@gmail.com>
2023-03-27 20:08:23 +0100

    "updated Chapter Fifteen"

---
+++ src/chapterFifteen/utils/AppUtil.java
@@ -0,0 +1,5 @@
+package chapterFifteen.utils;
+
+public class AppUtil {
+    public static String APP_ASSET_LOCATION = "C:\\Users\\Omisakin Joshua\\IdeaProjects\\folahanProject\\src\\chapterFifteen\\assets";
+}
