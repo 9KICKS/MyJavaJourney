0e1ee5ae47470f39b4f07771501ac1c4005de56d
9KICKS <officialjoshua9@gmail.com>
2023-03-29 23:46:40 +0100

    "updated Chapter Fifteen"

--- src/chapterFifteen/json/serialization/CatSerializer.java
+++ src/chapterFifteen/json/serialization/CatSerializer.java
@@ -4,10 +4,13 @@ import chapterFifteen.json.Cat;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.text.SimpleDateFormat;
+
 public class CatSerializer {
 
     public static String convertCatToJson(Cat cat) {
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
         try {
             return objectMapper.writeValueAsString(cat);
         } catch (JsonProcessingException e) {
