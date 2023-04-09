a2fd0c90689e638492dc1aa7e7e3fef03548ca69
9KICKS <officialjoshua9@gmail.com>
2023-04-09 17:04:05 +0100

    "updated AboutPizza.java"

--- src/Pizza/AboutPizza.java
+++ src/Pizza/AboutPizza.java
@@ -1,25 +1,7 @@
 package Pizza;
 
 public class AboutPizza {
-    private final int numberOfBoxes;
-    private final int eatenSlices;
-    private final int leftOvers;
-
-    public AboutPizza(int numberOfBoxes, int eatenSlices, int leftOvers) {
-        this.numberOfBoxes = numberOfBoxes;
-        this.eatenSlices = eatenSlices;
-        this.leftOvers = leftOvers;
-    }
-
-    public int getNumberOfBoxes() {
-        return numberOfBoxes;
-    }
-
-    public int getEatenSlices() {
-        return eatenSlices;
-    }
-
-    public int getLeftOvers() {
-        return leftOvers;
-    }
+    public int numberOfBoxes;
+    public int eatenSlices;
+    public int leftOvers;
 }
