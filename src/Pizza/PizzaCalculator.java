11ec041950ad0409eb8a517580e33304ea34ed18
9KICKS <officialjoshua9@gmail.com>
2023-04-09 17:05:53 +0100

    "updated PizzaCalculator.java"

--- src/Pizza/PizzaCalculator.java
+++ src/Pizza/PizzaCalculator.java
@@ -1,29 +1,32 @@
 package Pizza;
 
-public class PizzaCalculator {
-    public static void main(String[] args) {
-        int people = 45;
-        int numberOfMinimumSatisfaction = 2;
-        int numberOfMaximumSatisfaction = 3;
-        AboutPizza aboutPizza = calculatePizzaSlices(people, numberOfMinimumSatisfaction, numberOfMaximumSatisfaction);
-        System.out.println("Number of pizza's gotten: " + aboutPizza.getNumberOfBoxes());
-        System.out.println("Number of eaten pizza slices: " + aboutPizza.getEatenSlices());
-        System.out.println("Number of leftover pizza slices: " + aboutPizza.getLeftOvers());
-    }
+import java.util.Scanner;
 
-    public static AboutPizza calculatePizzaSlices(int people, int numberOfMinimumSatisfaction, int numberOfMaximumSatisfaction) {
-        int numberOfBoxes = (int) Math.ceil((double) people * numberOfMaximumSatisfaction / 8);
-        int numberOfSlices = numberOfBoxes * 8;
-        int numberOfMinimumSlices = numberOfMinimumSatisfaction * people;
-        int numberOfMaximumSlices = numberOfMaximumSatisfaction * people;
-        int numberOfLeftOverSlices = numberOfSlices - numberOfMaximumSlices;
-        if (numberOfLeftOverSlices < 0) {
-            numberOfLeftOverSlices = 0;
-        }
-        int numberOfEatenSlices = numberOfMaximumSlices;
-        if (numberOfEatenSlices < numberOfMinimumSlices) {
-            numberOfEatenSlices = numberOfMinimumSlices;
-        }
-        return new AboutPizza(numberOfBoxes, numberOfEatenSlices, numberOfLeftOverSlices);
+public class PizzaCalculator {
+    private final int minimumSatisfaction = 2;
+    private final int maximumSatisfaction = 3;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        PizzaCalculator calculator = new PizzaCalculator();
+        System.out.print("Enter the number of people that wants to eat the pizza: ");
+        int people = input.nextInt();
+        System.out.print("Enter the number of people that wants minimum satisfaction: ");
+        int minimum = input.nextInt();
+        System.out.print("Enter the number of people that wants maximum satisfaction: ");
+        int maximum = input.nextInt();
+
+        int minimumSlices = minimum * calculator.minimumSatisfaction;
+        int maximumSlices = maximum * calculator.maximumSatisfaction;
+        int totalSlices = people - minimum - maximum + minimumSlices + maximumSlices;
+
+        AboutPizza pizza = new AboutPizza();
+        pizza.numberOfBoxes = (int) Math.ceil(totalSlices / 8.0);
+        pizza.eatenSlices = totalSlices;
+        pizza.leftOvers = pizza.numberOfBoxes * 8 - totalSlices;
+
+        System.out.println("Number of boxes needed: " + pizza.numberOfBoxes);
+        System.out.println("Number of pizza slices eaten: " + pizza.eatenSlices);
+        System.out.println("Number of left overs: " + pizza.leftOvers);
     }
 }
