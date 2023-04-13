f2eff51a82a6f9ff800672e89e768abae58d8389
9KICKS <officialjoshua9@gmail.com>
2023-04-13 08:21:45 +0100

    "updated chapterSixteen"

---
+++ src/chapterSixteen/CollectionSample5.java
@@ -0,0 +1,18 @@
+package chapterSixteen;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CollectionSample5 {
+    public static void main(String[] args) {
+        Set<String> names = new HashSet<>();
+
+        names.add("Folahan");
+        names.add("Joshua");
+        names.add("Omisakin");
+        names.add("9KICKS!");
+
+        System.out.println(names);
+        System.out.println(names.toArray()[names.toArray().length - 4]);
+    }
+}
