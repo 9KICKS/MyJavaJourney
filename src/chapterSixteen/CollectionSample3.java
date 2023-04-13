6ef75152edcb104d38c51aa40541df3538aeff44
9KICKS <officialjoshua9@gmail.com>
2023-04-13 08:21:08 +0100

    "updated chapterSixteen"

---
+++ src/chapterSixteen/CollectionSample3.java
@@ -0,0 +1,17 @@
+package chapterSixteen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionSample3 {
+    public static void main(String[] args) {
+        List<Integer> digits = new ArrayList<>();
+        digits.add(10);
+        digits.add(20);
+        digits.add(30);
+        digits.add(40);
+
+        Integer number = digits.get(2);
+        System.out.println(number);
+    }
+}
