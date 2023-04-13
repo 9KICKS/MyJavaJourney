90311ecfcd2c0c2f12e218bfb380ad0409c4c15a
9KICKS <officialjoshua9@gmail.com>
2023-04-13 08:22:05 +0100

    "updated chapterSixteen"

---
+++ src/chapterSixteen/CollectionSample6.java
@@ -0,0 +1,14 @@
+package chapterSixteen;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CollectionSample6 {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 10;
+        numbers[1] = 11;
+        numbers[3] = 13;
+        Set<String> names = new HashSet<>();
+    }
+}
