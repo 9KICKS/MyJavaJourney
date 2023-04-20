ec27ba720894b3b3e5f9ff2b72c6b615fb2c2f93
9KICKS <officialjoshua9@gmail.com>
2023-04-20 14:02:28 +0100

    "added QueueSample.java"

---
+++ src/chapterSixteen/queue/QueueSample.java
@@ -0,0 +1,22 @@
+package chapterSixteen.queue;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueSample {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new PriorityQueue<>(3);
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        queue.offer(4);
+        queue.offer(5);
+        System.out.println(queue.poll());
+        System.out.println(queue.peek());
+        System.out.println(queue);
+        System.out.println(queue.poll());
+        System.out.println(queue.peek());
+        System.out.println(queue);
+        System.out.println(queue.element());
+    }
+}
