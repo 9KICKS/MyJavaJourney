592fb2dc3e0aabe3d25f921cc89425e3d1103840
9KICKS <officialjoshua9@gmail.com>
2023-05-01 23:59:11 +0100

    "added FilterOutAllNegativeElementsInArray.java"

---
+++ src/selfStudy/FilterOutAllNegativeElementsInArray.java
@@ -0,0 +1,12 @@
+package selfStudy;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class FilterOutAllNegativeElementsInArray {
+    public static void main(String[] args) {
+        int[] numbers = {5, 3, 2, 1, 39, 44, -23, -23};
+        System.out.println("Array without the negative elements = " + Arrays.toString(IntStream.of(numbers)
+                .filter(x -> x >= 0).toArray()));
+    }
+}
