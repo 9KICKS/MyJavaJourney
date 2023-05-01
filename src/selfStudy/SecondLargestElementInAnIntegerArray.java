67fb782d25da06940464c9603bb0c34b210a2cc7
9KICKS <officialjoshua9@gmail.com>
2023-05-02 00:00:47 +0100

    "added SecondLargestElementInAnIntegerArray.java"

---
+++ src/selfStudy/SecondLargestElementInAnIntegerArray.java
@@ -0,0 +1,18 @@
+package selfStudy;
+
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public class SecondLargestElementInAnIntegerArray {
+    public static void main(String[] args) {
+        int[] numbers = {100, 37, 383, 389, 32, 28, 292, 1003};
+        int secondLargest = IntStream.of(numbers)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .orElse(-1);
+
+        System.out.println("Second largest element: " + secondLargest);
+    }
+}
