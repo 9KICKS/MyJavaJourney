bcb7b5a16f1432d0a5d05dd6ac9b9e65eeeb8309
9KICKS <officialjoshua9@gmail.com>
2023-05-01 23:59:48 +0100

    "added NumberOfElementsGreaterThanTheAverage.java"

---
+++ src/selfStudy/NumberOfElementsGreaterThanTheAverage.java
@@ -0,0 +1,14 @@
+package selfStudy;
+
+import java.util.stream.IntStream;
+
+public class NumberOfElementsGreaterThanTheAverage {
+    public static void main(String[] args) {
+        int[] numbers = {5, 3, 2, 1, 39, 44, -23, -23};
+        double average = IntStream.of(numbers).average().getAsDouble();
+        System.out.println("The average = " + average);
+        IntStream.of(numbers)
+                .filter(x -> x > average)
+                .forEach(x -> System.out.println(x + " > average"));
+    }
+}
