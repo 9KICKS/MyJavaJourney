1339ae3e5bdeaf4d52503ff1a65f13b3fbffe9e0
9KICKS <officialjoshua9@gmail.com>
2023-05-04 07:52:33 +0100

    "added StringsThatStartWithASpecificLetter.java"

---
+++ src/selfStudy/StringsThatStartWithASpecificLetter.java
@@ -0,0 +1,15 @@
+package selfStudy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class StringsThatStartWithASpecificLetter {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList("apple", "banana", "pear", "grape"));
+        ArrayList<String> result = arrayList.stream()
+                .filter(word -> word.startsWith("p"))
+                .collect(Collectors.toCollection(ArrayList::new));
+        System.out.println("Strings that start with 'p' = " + result);
+    }
+}
