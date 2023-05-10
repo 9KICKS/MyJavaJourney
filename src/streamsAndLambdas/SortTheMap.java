0a21e356be5f18907e1e5cd45c41281763b22c15
9KICKS <officialjoshua9@gmail.com>
2023-05-10 17:28:12 +0100

    "added SortTheMap.java"

--- src/streamsAndLambdas/SortTheMap.java
+++ src/streamsAndLambdas/SortTheMap.java
@@ -7,15 +7,15 @@ public class SortTheMap {
     public static void main(String[] args) {
         Map<String, Integer> myMap = new HashMap<>();
         myMap.put("Folahan", 21);
-        myMap.put("Renike", 22);
-        myMap.put("Zainab", 22);
+        myMap.put("B", 22);
+        myMap.put("AB", 22);
         myMap.put("Tayo", 24);
 
-
         Map<String, Integer> sorted = myMap.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
+                .sorted(Map.Entry.<String, Integer>comparingByValue()
+                        .thenComparing(Map.Entry.comparingByKey()))
                 .collect(Collectors.toMap(
-                        Map.Entry::getKey,
+                        stringIntegerEntry -> stringIntegerEntry.getKey(),
                         Map.Entry::getValue,
                         (x, y) -> x, LinkedHashMap::new));
         System.out.println(sorted);
